93686fc001586c95b09a5102219e96ebed4d7099
Igor Belogurov <belogurovigor@gmail.com>
2020-02-08 22:17:43 +0300

    Добавляет Enzyme и e2e тест для компонента Main

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -3,20 +3,22 @@ import renderer from 'react-test-renderer';
 
 import Main from './main';
 
-const cities = [
-  {
-    "id": 962,
-    "city": `Lushuihe`,
-    "country": `China`
-  }, {
-    "id": 605,
-    "city": `Nong Ruea`,
-    "country": `Thailand`
-  }
-];
+const defaultProps = {
+  data: [
+    {
+      "id": 962,
+      "city": `Lushuihe`,
+      "country": `China`
+    }, {
+      "id": 605,
+      "city": `Nong Ruea`,
+      "country": `Thailand`
+    }
+  ]
+};
 
 it(`<Main /> renders correctly`, () => {
-  const component = renderer.create(<Main data={cities} />).toJSON();
+  const component = renderer.create(<Main {...defaultProps} />).toJSON();
 
   expect(component).toMatchSnapshot();
 });
