77d584435a3652254d71558e12daec457fdbc221
Igor Belogurov <belogurovigor@gmail.com>
2020-03-25 23:11:27 +0300

    Добавляет тесты для App и reducer

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import renderer from 'react-test-renderer';
 
 import App from './app';
 
 import {offers, reviews} from './mock';
 
+export const initialState = {
+  selectedCity: `Amsterdam`,
+  currentOffer: 1,
+  offers,
+  reviews,
+};
+
+const mockStore = configureStore([thunk]);
+const store = mockStore(initialState);
+
 it(`<App /> renders correctly`, () => {
   const component = renderer.create(
-      <App offers={offers} reviews={reviews} />,
+      <Provider store={store}>
+        <App offers={offers} reviews={reviews} />
+      </Provider>,
       {
         createNodeMock: () => document.createElement(`div`)
       }
--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -1,5 +1,11 @@
+// import configureMockStore from 'redux-mock-store';
+// import thunk from 'redux-thunk';
+
 import reducer, {initialState} from "./reducer";
-import {ActionTypes} from "./actions";
+import {ActionTypes, ActionCreators} from "./actions";
+
+// const middlewares = [thunk];
+// const mockStore = configureMockStore(middlewares);
 
 it(`Reducer without additional parameters should return initial state`, () => {
   expect(reducer(void 0, {})).toEqual(initialState);
@@ -43,21 +49,28 @@ it(`Reducer should set City Name string`, () => {
 });
 
 describe(`Action Creators work correctly`, () => {
-  it(`setCurrentOffer Action Creator for incrementing step returns correct action`, () => {
-    // expect(ActionCreators.setCurrentOffer(1)).toEqual({
-    //   type: ActionTypes.SET_CURRENT_OFFER,
-    //   payload: {
-    //     id: 1
-    //   },
-    // });
+
+  it(`setCurrentOffer Action Creator for setting current offer returns correct action`, () => {
+    const dispatch = jest.fn();
+    const action = ActionCreators.setCurrentOffer(1)(dispatch);
+
+    expect(action).toEqual(dispatch({
+      type: ActionTypes.SET_CURRENT_OFFER,
+      payload: {
+        id: 1
+      },
+    }));
   });
 
-  it(`setSelectedCity Action Creator for incrementing step returns correct action`, () => {
-    // expect(ActionCreators.setSelectedCity('City Name')).toEqual({
-    //   type: ActionTypes.SET_SELECTED_CITY,
-    //   payload: {
-    //     name: 'City Name
-    //   },
-    // });
+  it(`setSelectedCity Action Creator for setting selected city returns correct action`, () => {
+    const dispatch = jest.fn();
+    const action = ActionCreators.setSelectedCity(`City`)(dispatch);
+
+    expect(action).toEqual(dispatch({
+      type: ActionTypes.SET_SELECTED_CITY,
+      payload: {
+        name: `City`
+      },
+    }));
   });
 });
